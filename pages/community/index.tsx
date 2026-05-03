afa1647cacacead1cfe03e34e55b652a223568a6
Ali <nmuhammadali2001@gmail.com>
2026-05-03 19:09:51 +0900

    feat: integration Header and Footer visa Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,16 +1,22 @@
-"use client";
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
+const Community: NextPage = () => {
+    console.log("COMMUNITY COMPONENT - PAGES ROUTER");
     const [title, setTitle] = useState<string>("hello");
+
     return (
         <div>
             COMMUNITY{" "}
-            <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+            <button 
+                onClick={() => alert("Hello MIT")} 
+                style={{ margin: "15px" }}
+            >
                 PressMe
             </button>
         </div>
     );
 };
 
-export default Community;
+export default withLayoutBasic(Community);
